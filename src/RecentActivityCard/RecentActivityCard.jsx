183c4dcfc04baea10dc231eb7162e9d743b46d83
beata-teck <batatefera7@gmail.com>
2025-10-04 19:19:17 -0800

    changing some props

--- src/RecentActivityCard/RecentActivityCard.jsx
+++ src/RecentActivityCard/RecentActivityCard.jsx
@@ -1,12 +1,13 @@
-import './RecentActivityCard.css'
+import './RecentActivityCard.css';
 
-export default function RecentActivityCard({person, deed, thing, time}) {
-    return (
-        <div className='activity-card'>
-            <p className='activity'>
-                <span className='person-name'>{person}</span> {deed} <span className='thing-name'>{thing}</span>
-            </p>
-            <p className='time'>{time + ' ago'}</p>
-        </div>
-    )
+export default function RecentActivityCard({ person, deed, thing, time }) {
+  return (
+    <div className="activity-card">
+      <p className="activity">
+        <span className="person-name">{person}</span> {deed}{' '}
+        <span className="thing-name">{thing}</span>
+      </p>
+      <p className="time">{time + ' ago'}</p>
+    </div>
+  );
 }
--- src/TeamCard/TeamCard.jsx
+++ src/TeamCard/TeamCard.jsx
@@ -1,13 +1,15 @@
-import './TeamCard.css'
+import './TeamCard.css';
 
-export default function TeamCard({team_name, role, avatar}) {
-    return (
-        <div className='member-container'>
-            <img className='team-avatar' src={avatar} alt={'Picture of ' + team_name} />
-            <div className='team-info'>
-                <h3>{team_name}</h3>
-                <p>{role}</p>
-            </div>
-        </div>
-    )
+export default function TeamCard({ team_name, role, avatar }) {
+  const avatarSrc = avatar || 'https://via.placeholder.com/80?text=?';
+
+  return (
+    <div className="member-container">
+      <img className="team-avatar" src={avatarSrc} alt={team_name} />
+      <div className="team-info">
+        <h3>{team_name}</h3>
+        <p>{role}</p>
+      </div>
+    </div>
+  );
 }
